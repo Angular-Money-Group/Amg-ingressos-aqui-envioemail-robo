647bd9a83c5936e79d512cd60678bf8dc2da597f
roberthsx <roberthenrrique@gmail.com>
2024-01-16 09:57:14 -0300

    setup

---
+++ setup-and-run.sh
@@ -0,0 +1,6 @@
+git switch develop
+git pull
+docker container stop apienvioemail
+docker container rm -f apienvioemail
+docker build  -t apienvioemail-v1 .
+docker run -t -i --rm api-eventos --name apienvioemail apienvioemail-v1
