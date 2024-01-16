23b2e038c80a3af5297057a0fdf021820aba01ff
roberthsx <roberthenrrique@gmail.com>
2024-01-16 10:26:48 -0300

    ajuste de docker

--- setup-and-run.sh
+++ setup-and-run.sh
@@ -3,4 +3,4 @@ git pull
 docker container stop apienvioemail
 docker container rm -f apienvioemail
 docker build  -t apienvioemail-v1 .
-docker run -t -i --rm --name apienvioemail apienvioemail-v1
+docker run --name apienvioemail apienvioemail-v1
