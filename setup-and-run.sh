ffb3b751c464d0e847bf72ad9c063ab4ab7f4cd8
roberthsx <roberthenrrique@gmail.com>
2024-01-16 10:32:33 -0300

    backgroud

--- setup-and-run.sh
+++ setup-and-run.sh
@@ -3,4 +3,4 @@ git pull
 docker container stop apienvioemail
 docker container rm -f apienvioemail
 docker build  -t apienvioemail-v1 .
-docker run --name apienvioemail apienvioemail-v1
+docker run -d --name apienvioemail apienvioemail-v1
